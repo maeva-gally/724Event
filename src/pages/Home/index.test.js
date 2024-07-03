1b08d54a6dc550a1f2bc3719b48956c95fd68b76
maeva-gally <piti-bichoco@hotmail.fr>
2024-07-03 04:14:37 +0200

    initial upload

---
+++ src/pages/Home/index.test.js
@@ -0,0 +1,55 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "./index";
+
+describe("When Form is created", () => {
+  it("a list of fields card is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Email");
+    await screen.findByText("Nom");
+    await screen.findByText("Prénom");
+    await screen.findByText("Personel / Entreprise");
+  });
+
+  describe("and a click is triggered on the submit button", () => {
+    it("the success message is displayed", async () => {
+      render(<Home />);
+      fireEvent(
+        await screen.findByText("Envoyer"),
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      await screen.findByText("En cours");
+      await screen.findByText("Message envoyé !");
+    });
+  });
+
+});
+
+
+describe("When a page is created", () => {
+  it("a list of events is displayed", () => {
+    render(<Home />);
+    
+    const eventsElement = screen.getByTestId("events-testid");
+    expect(eventsElement).toBeInTheDocument();
+
+  })
+  it("a list a people is displayed", () => {
+    render(<Home />);
+    const peopleElements = screen.getAllByTestId('card-image-people-testid');
+    expect(peopleElements.length).toEqual(6);
+  })
+  it("a footer is displayed", () => {
+    render(<Home />);
+    const footerElement = screen.getByTestId("footer-testid");
+    expect(footerElement).toBeInTheDocument();
+
+  })
+  it("an event card, with the last event, is displayed", () => {
+    render(<Home />);
+    const lastEvent = screen.getByTestId("presta-testid");
+    expect(lastEvent).toBeInTheDocument();
+  })
+});
